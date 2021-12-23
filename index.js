1e50e963cdbfc2b8b4882ac7ef45ba98843d4781
bradleyjosephh <bradleyjosephh@gmail.com>
2021-12-22 19:19:38 -0800

    add functions and comments

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function promptOptions() {
     inquirer.prompt([
         {
             type: "list",
-            choices: ["viewEmployee", "viewRole", "addEmployee", "viewDeparment", "addDepartment", "addRole", "updateRole","Exit App"],
+            choices: ["viewEmployee", "viewRole", "addEmployee", "viewDeparment", "addDepartment", "addRole", "updateRole", "removeEmployee", "removeDepartment", "removeRole", "Exit App"],
             messages: "What would you like to do?",
             name: "name",
         }
@@ -26,7 +26,7 @@ function promptOptions() {
                 viewRole()
                 break;
             case "viewDepartment":
-                viewDepatment()
+                viewDepartment()
                 break;
             case "addEmployee":
                 addEmployee()
@@ -40,36 +40,126 @@ function promptOptions() {
             case "updateRole":
                 updateRole()
                 break;
+            case 'removeDepartment':
+                removeDepartment();
+                break;
+            case 'removeRole':
+                removeRoles();
+                break;
+            case 'removeEmployee':
+                removeEmployees();
+                break;
             default:
                 process.exit(0)
         }
     })
 }
-
-function viewEmployee(){
-    db.query("SELECT * FROM EMPLOYEE", function(err,data){
-        if(err) throw err;
+// view functions
+function viewEmployee() {
+    db.query("SELECT * FROM EMPLOYEE", function (err, data) {
+        if (err) throw err;
         console.table(data);
         promptOptions()
     })
 }
 
 
-function viewDeparment(){
-    db.query("SELECT * FROM DEPARTMENT", function(err,data){
-        if(err) throw err;
+function viewDepartment() {
+    db.query("SELECT * FROM DEPARTMENT", function (err, data) {
+        if (err) throw err;
         console.table(data);
         promptOptions()
     })
 }
 
 
-function viewRole(){
-    db.query("SELECT * FROM ROLES", function(err,data){
-        if(err) throw err;
+function viewRole() {
+    db.query("SELECT * FROM ROLES", function (err, data) {
+        if (err) throw err;
         console.table(data);
         promptOptions()
     })
 }
 
 promptOptions()
+
+// add functions
+
+function addDepartment() {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Please input name of new department.'
+      }
+    ])
+      .then(newDepartment => {
+        db.query('INSERT INTO department SET name= ?', newDepartment.name, err => {
+          if (err) { console.log(err) }
+          console.log(`${newDepartment.name} has been added!`)
+          start()
+        })
+      })
+  }
+  
+  function addRole() {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'Please input name of new role.'
+      },
+      {
+        type: 'input',
+        name: 'department_id',
+        message: 'What is the department ID?'
+      },
+      {
+        type: 'input',
+        name: 'salary',
+        message: `Please input the new role's salary.`
+      }
+    ])
+      .then(newRole => {
+        db.query('INSERT INTO role SET ?', newRole, err => {
+          if (err) { console.log(err) }
+          console.log(`${newRole.title} has been added!`)
+          promptOptions()
+        })
+      })
+  }
+  
+  function addEmployee() {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'first_name',
+        message: 'What is the FIRST name of the employee?'
+      },
+      {
+        type: 'input',
+        name: 'last_name',
+        message: 'What is the LAST name of the employee?'
+      },
+      {
+        type: 'input',
+        name: 'roles_id',
+        message: `What is the new employee's role ID?`
+      },
+      {
+        type: 'number',
+        name: 'manager_id',
+        message: `Please input Manager ID.`
+      }
+    ])
+      .then(newEmployee => {
+        db.query('INSERT INTO employee SET ?', newEmployee, err => {
+          if (err) { console.log(err) }
+          console.log(`${newEmployee.first_name} ${newEmployee.last_name} is now added!`)
+          promptOptions()
+        })
+      })
+  }
+
+//   update functions
+
